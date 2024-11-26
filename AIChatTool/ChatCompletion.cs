c6fb4c5a1b5c12ace8e02463b9f8f1960e5534b2
wenchao <susices@hotmail.com>
2024-11-27 00:20:48 +0800

    commit

--- AIChatTool/ChatCompletion.cs
+++ AIChatTool/ChatCompletion.cs
@@ -1,6 +1,75 @@
-﻿namespace AIChatTool;
+﻿
 
-public class ChatCompletion
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace AIChatTool
 {
-    
+    public class ChatCompletion
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("object")]
+        public string? Object { get; set; }
+
+        [JsonPropertyName("created")]
+        public long Created { get; set; }
+
+        [JsonPropertyName("model")]
+        public string? Model { get; set; }
+
+        [JsonPropertyName("choices")]
+        public List<Choice>? Choices { get; set; }
+
+        [JsonPropertyName("usage")]
+        public Usage? Usage { get; set; }
+
+        [JsonPropertyName("system_fingerprint")]
+        public string? SystemFingerprint { get; set; }
+    }
+
+    public class Choice
+    {
+        [JsonPropertyName("index")]
+        public int Index { get; set; }
+
+        [JsonPropertyName("message")]
+        public Message Message { get; set; }
+
+        [JsonPropertyName("logprobs")]
+        public object Logprobs { get; set; }
+
+        [JsonPropertyName("finish_reason")]
+        public string FinishReason { get; set; }
+    }
+
+    public class Message
+    {
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+
+    public class Usage
+    {
+        [JsonPropertyName("prompt_tokens")]
+        public int PromptTokens { get; set; }
+
+        [JsonPropertyName("completion_tokens")]
+        public int CompletionTokens { get; set; }
+
+        [JsonPropertyName("total_tokens")]
+        public int TotalTokens { get; set; }
+
+        [JsonPropertyName("prompt_cache_hit_tokens")]
+        public int PromptCacheHitTokens { get; set; }
+
+        [JsonPropertyName("prompt_cache_miss_tokens")]
+        public int PromptCacheMissTokens { get; set; }
+    }
 }
--- AIChatTool/ConfigUtil.cs
+++ AIChatTool/ConfigUtil.cs
@@ -2,5 +2,5 @@
 
 public class ConfigUtil
 {
-    
+    public const string API_KEY = "sk-d484a5253df44cba8435ffdfab8430d3";
 }
